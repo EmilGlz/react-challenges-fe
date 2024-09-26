dbab11f3650f06cae85e3d11e8e4c6a74b4a8f14
EmilGlz <emilquluzade2001@gmail.com>
2024-09-26 17:21:41 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer"
+import Navbar from "./components/Navbar"
+
+function App() {
+  return (
+    <>
+    <Navbar name="" surname="" />
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Footer />
+    </>
+  )
+}
+
+export default App
